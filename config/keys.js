d396239f041cb1de42338d56d4b020226687b48b
hai2kk <hai2kk@gmail.com>
2017-12-04 22:09:51 -0500

    Modified project directory structure
    Modified project directory structure

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports  = {
+    googleClientID  : "733213140234-k2du6mub02f1bium89gq0v654p1gh6ob.apps.googleusercontent.com",
+    googleClientSecret : "EdirSV3tq1WcqZ_QJrq-By0i"
+}
--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
 const app = express();
-passport.use(new GoogleStrategy());
-
-app.get("/", (req, res) => {
-  res.send({ message: "hello there. Heroku here!" });
-});
+require("./services/passport");
+require("./routes/authRoute")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
